74d63a0bf850e2917eab4bc3aacc06a75ff8d345
chriswebdv <chriswebdv@gmail.com>
2025-02-23 18:57:14 -0600

    add functions to add the various points

--- Project-1/main.js
+++ Project-1/main.js
@@ -1,2 +1,13 @@
-let num1 = 0;
-let num2 = 0;
+let scoreBtn = document.querySelector('.score-btn');
+let scoreBoard = document.querySelector('.score-board');
+scoreBoard.textContent = 0;
+
+function addTwoPoints() {
+  return (scoreBoard.textContent += 2);
+}
+function addThreePoints() {
+  return (scoreBoard.textContent += 3);
+}
+function addSevenPoints() {
+  return (scoreBoard.textContent += 7);
+}
